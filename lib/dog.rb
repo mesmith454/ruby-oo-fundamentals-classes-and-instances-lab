9fce35a83dc8e383610f60a0f4e672cdc18b0b3f
mesmith454 <mesmith454@gmail.com>
2020-11-17 10:32:08 -0800

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,7 @@ class Dog
     #code
 end
 
-Fido = Dog.new
-Snoopy = Dog.new
-Lassie = Dog.new
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
 
-class Person
-    #code
-end
-
-adele_goldberg = Person.new
-alan_kay = Person.new 
